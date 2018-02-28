1ecd4e1e313ace972413915264579903d371fbcd
Dot <dotcra@gmail.com>
2018-02-28 22:07:42 +0800

    add group by property, o_serve,investby in stat

--- stat.php
+++ stat.php
@@ -4,15 +4,29 @@
  */
 $sql = "select count(pid) a from projects";
 $count = (new Db)->query($sql)['a'];
+
 // group by type
 // $sql = "select type,count(pid) count,count(pid)/(select count(pid) from projects) ratio from projects group by type";
 $sql = "select type,count(pid) count from projects group by type";
 $t_rows=(new Db)->query($sql);
 
+// group by property
+$sql = "select property,count(pid) count from projects group by property";
+$p_rows=(new Db)->query($sql);
+
 // group by oname
 $sql = "select oname,count(pid) count from projects p join organization o on o.oid=p.oid group by p.oid";
 $o_rows=(new Db)->query($sql);
 
+// group by oname_serve
+$sql = "select oname,count(pid) count from projects p join organization o on o.oid=p.oid_serve group by p.oid_serve";
+$s_rows=(new Db)->query($sql);
+
+// group by investby
+$sql = "select investby,count(pid) count from projects group by investby";
+$i_rows=(new Db)->query($sql);
+
+// group by investment
 $sql = "select count(investment) count from projects where investment < 10000";
 $a=(new Db)->query($sql);
 $sql = "select count(investment) count from projects where investment between 10000 and 49999";
@@ -63,7 +77,103 @@ $f=(new Db)->query($sql);
 			<table class="table table-sm table-striped table-bordered">
 				<thead class="thead-light">
 					<tr>
-						<th class="w-25" scope="col">投资类型</th>
+						<th class="w-25" scope="col">建设性质</th>
+						<th class="w-25" scope="col">项目个数</th>
+						<th class="w-25" scope="col">占 比</th>
+					</tr>
+				</thead>
+				<tbody>
+<?php foreach($p_rows as $v): ?>
+					<tr>
+						<td><?= $v['property'] ?></td>
+						<td><?= $v['count'] ?></td>
+						<td><?= $v['count']/$count*100 . '%' ?></td>
+					</tr>
+<?php endforeach ?>
+					<tr class="font-weight-bold">
+						<td>合 计</td>
+						<td><?= $count ?></td>
+						<td></td>
+					</tr>
+				</tbody>
+			</table>
+
+			<table class="table table-sm table-striped table-bordered">
+				<thead class="thead-light">
+					<tr>
+						<th class="w-25" scope="col">责任单位</th>
+						<th class="w-25" scope="col">项目个数</th>
+						<th class="w-25" scope="col">占 比</th>
+					</tr>
+				</thead>
+				<tbody>
+<?php foreach($o_rows as $v): ?>
+					<tr>
+						<td><?= $v['oname'] ?></td>
+						<td><?= $v['count'] ?></td>
+						<td><?= $v['count']/$count*100 . '%' ?></td>
+					</tr>
+<?php endforeach ?>
+					<tr class="font-weight-bold">
+						<td>合 计</td>
+						<td><?= $count ?></td>
+						<td></td>
+					</tr>
+				</tbody>
+			</table>
+
+			<table class="table table-sm table-striped table-bordered">
+				<thead class="thead-light">
+					<tr>
+						<th class="w-25" scope="col">服务单位</th>
+						<th class="w-25" scope="col">项目个数</th>
+						<th class="w-25" scope="col">占 比</th>
+					</tr>
+				</thead>
+				<tbody>
+<?php foreach($s_rows as $v): ?>
+					<tr>
+						<td><?= $v['oname'] ?></td>
+						<td><?= $v['count'] ?></td>
+						<td><?= $v['count']/$count*100 . '%' ?></td>
+					</tr>
+<?php endforeach ?>
+					<tr class="font-weight-bold">
+						<td>合 计</td>
+						<td><?= $count ?></td>
+						<td></td>
+					</tr>
+				</tbody>
+			</table>
+
+			<table class="table table-sm table-striped table-bordered">
+				<thead class="thead-light">
+					<tr>
+						<th class="w-25" scope="col">投资主体</th>
+						<th class="w-25" scope="col">项目个数</th>
+						<th class="w-25" scope="col">占 比</th>
+					</tr>
+				</thead>
+				<tbody>
+<?php foreach($i_rows as $v): ?>
+					<tr>
+						<td><?= $v['investby'] ?></td>
+						<td><?= $v['count'] ?></td>
+						<td><?= $v['count']/$count*100 . '%' ?></td>
+					</tr>
+<?php endforeach ?>
+					<tr class="font-weight-bold">
+						<td>合 计</td>
+						<td><?= $count ?></td>
+						<td></td>
+					</tr>
+				</tbody>
+			</table>
+
+			<table class="table table-sm table-striped table-bordered">
+				<thead class="thead-light">
+					<tr>
+						<th class="w-25" scope="col">总投资</th>
 						<th class="w-25" scope="col">项目个数</th>
 						<th class="w-25" scope="col">占 比</th>
 					</tr>
@@ -106,30 +216,5 @@ $f=(new Db)->query($sql);
 					</tr>
 				</tbody>
 			</table>
-
-			<table class="table table-sm table-striped table-bordered">
-				<thead class="thead-light">
-					<tr>
-						<th class="w-25" scope="col">责任单位</th>
-						<th class="w-25" scope="col">项目个数</th>
-						<th class="w-25" scope="col">占 比</th>
-					</tr>
-				</thead>
-				<tbody>
-<?php foreach($o_rows as $v): ?>
-					<tr>
-						<td><?= $v['oname'] ?></td>
-						<td><?= $v['count'] ?></td>
-						<td><?= $v['count']/$count*100 . '%' ?></td>
-					</tr>
-<?php endforeach ?>
-					<tr class="font-weight-bold">
-						<td>合 计</td>
-						<td><?= $count ?></td>
-						<td></td>
-					</tr>
-				</tbody>
-			</table>
-
 		  </main>
 		</div>
