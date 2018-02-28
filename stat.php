b6609b4bc149eeef739918af0fb840b0bb79f274
Dot <dotcra@gmail.com>
2018-02-28 17:46:26 +0800

    nth

--- stat.php
+++ stat.php
@@ -9,18 +9,6 @@ $rows=(new Db)->query($sql);
 		    <a role="button" class="btn btn-danger text-white" href="<?= "$root/$controller/$method/allprog" ?>">进度月报</a>
 		  </div>
 		  <div class="col-auto">
-			<div class="dropdown" id="dates">
-					  <button class="btn btn-info dropdown-toggle" type="button">
-						  <?= date('Y-m') ?>
-					  </button>
-					  <div class="dropdown-menu">
-<?php for($i=date('n'); date('n') - $i < 12; $i--): ?>
-						<a class="dropdown-item <?php if(date('n') == $i) echo 'active' ?>" href="#"><?= date('Y-m', mktime(0,0,0,$i,1)) ?></a>
-<?php endfor ?>
-					  </div>
-			</div>
-		  </div>
-		  <div class="col-auto">
 		    <button type="button" class="btn btn-info">导出报表</button>
 		  </div>
 		  </div>
@@ -30,7 +18,7 @@ $rows=(new Db)->query($sql);
 					<tr>
 						<th scope="col">项目类型</th>
 						<th scope="col">项目个数</th>
-						<th scope="col">占比</th>
+						<th scope="col">占 比</th>
 					</tr>
 				</thead>
 				<tbody>
@@ -71,7 +59,7 @@ $rows=(new Db)->query($sql);
 					<tr>
 						<th scope="col">投资规模</th>
 						<th scope="col">项目个数</th>
-						<th scope="col">占比</th>
+						<th scope="col">占 比</th>
 					</tr>
 				</thead>
 				<tbody>
@@ -112,7 +100,7 @@ $rows=(new Db)->query($sql);
 					<tr>
 						<th scope="col">责任单位</th>
 						<th scope="col">项目个数</th>
-						<th scope="col">占比</th>
+						<th scope="col">占 比</th>
 					</tr>
 				</thead>
 				<tbody>
