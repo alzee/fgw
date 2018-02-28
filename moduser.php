fc528b63079c8ca2b13b2e472af5701702c608c4
Dot <dotcra@gmail.com>
2018-02-28 11:04:28 +0800

    moduser done

--- moduser.php
+++ moduser.php
@@ -3,31 +3,55 @@
 $uid = $_SESSION['uid'];
 $uname=$_SESSION['uname'];
 $uname1=$parameter;
-if(empty($uname1)){
-	$uname1 = $uname;
+if($uname == $uname1){
+	$disabled = 'disabled';
 }
 
 if($_POST){
-	if(isset($_POST['oid'])){
-		echo $_POST['oid'];
+	if(!isset($disabled)){
+		$cols='';
+		if(isset($_POST['oid']) && $_POST['oid'] != 0){
+			$cols .= "oid={$_POST['oid']},";
+		}
+		if(isset($_POST['rid']) && $_POST['rid'] != 0){
+			$cols .= "rid={$_POST['rid']},";
+		}
+		if(!empty($_POST['newpw1'])){
+			$newpw = md5($_POST['newpw1']);
+			$cols .= "passwd='$newpw',";
+		}
+		if(!empty($cols)){
+			$cols = rtrim($cols, ',');
+			$sql = "update users set $cols where uname='$uname1'";
+			(new Db)->query($sql);
+		}
+
+		header("Location: $root/$controller/$method/$parameter");
+		exit;
 	}
-	if(isset($_POST['rid'])){
-		echo $_POST['rid'];
-	}
-	if(isset($_POST['newpw1'])){
-		echo $_POST['newpw1'];
+	else{
+		$err = '你不能改自己的哦!';
 	}
 }
 
-// prepare data
+/*
+ * prepare date
+ * 
+ */
+// user organization and role
 $sql = "select uname,oname,rname from users u,organization o,role r where uname='$uname1' and o.oid=u.oid and u.rid=r.rid";
 $u_row = (new Db)->query($sql);
 
-$sql = "select * from organization";
-$o_rows = (new Db)->query($sql);
+// if is myself, we don't need to query sql for the lists since we never use it
+if(!isset($disabled)){
+	// organization list for select options;
+	$sql = "select * from organization";
+	$o_rows = (new Db)->query($sql);
 
-$sql = "select rid,rname from role";
-$r_rows = (new Db)->query($sql);
+	// role list for select options;
+	$sql = "select rid,rname from role";
+	$r_rows = (new Db)->query($sql);
+}
 ?>
 		<section class="row">
 			<aside class="col-md-2">
@@ -59,31 +83,35 @@ $r_rows = (new Db)->query($sql);
 			  <div class="input-group-prepend">
 				  <span class="input-group-text">机 构</span>
 			  </div>
-			  <select class="custom-select" name="oid">
+			  <select class="custom-select" name="oid" <?= $disabled ?>>
 				<option value="0"><?= $u_row['oname'] ?></option>
+<?php if(!isset($disabled)): ?>
 <?php foreach($o_rows as $v): ?>
 				<option value="<?= $v['oid'] ?>"><?= $v['oname'] ?></option>
 <?php endforeach ?>
+<?php endif ?>
 			  </select>
 		  </div>
 		  <div class="input-group mb-3 col-sm-5 mx-auto">
 			  <div class="input-group-prepend">
 				  <span class="input-group-text">角 色</span>
 			  </div>
-			  <select class="custom-select" name="rid">
+			  <select class="custom-select" name="rid" <?= $disabled ?>>
 				<option value="0"><?= $u_row['rname'] ?></option>
+<?php if(!isset($disabled)): ?>
 <?php foreach($r_rows as $v): ?>
 				<option value="<?= $v['rid'] ?>"><?= $v['rname'] ?></option>
 <?php endforeach ?>
+<?php endif ?>
 			  </select>
 		  </div>
 		  <div class="input-group mb-3 col-sm-5 mx-auto">
 			  <div class="input-group-prepend">
 				  <span class="input-group-text">新密码</span>
 			  </div>
-			  <input type="password" class="form-control" name="newpw1">
+			  <input type="password" class="form-control" name="newpw1" <?= $disabled ?>>
 		  </div>
-		  <button type="submit" class="btn btn-success d-block mx-auto">提 交</button>
+		  <button type="submit" class="btn btn-success d-block mx-auto" <?= $disabled ?>>提 交</button>
 		  </form>
 
 		  </main>
