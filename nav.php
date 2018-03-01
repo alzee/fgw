517a62bf986d150944884d5e00342cf4a932a210
Dot <dotcra@gmail.com>
2018-03-01 10:37:55 +0800

    use flex instead of grid for admin nav

--- nav.php
+++ nav.php
@@ -2,12 +2,12 @@
 $rid=$_SESSION['rid'];
 ?>
 	  <div class="container" id="<?= $controller ?>">
-		  <nav class="row mx-0 mb-3" id="nav">
-			  <ol class="breadcrumb col-auto col-sm mb-0">
+		  <nav class="mx-0 mb-3 d-flex justify-content-between flex-wrap" id="nav">
+			  <ol class="breadcrumb mb-0">
 			  <li class="breadcrumb-item"><a href="<?= "$root/home" ?>">首 页</a></li>
 				  <li class="breadcrumb-item active" aria-current="page">设 置</li>
 			  </ol>
-			  <div class="col-auto">
+			  <div class="mx-3">
 			  <div class="btn-group btn-group-sm mb-2 my-2" role="group">
 				  <a role="button" href="<?= $root ?>/admin/chpwd" class="btn btn-danger <?php if($method=='chpwd' || $method == "") echo 'active'; ?>">修改密码</a>
 <?php if($rid==3): ?>
