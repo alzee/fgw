ae7e1d788b5633c32da0a34d0d94b994be63a421
Dot <dotcra@gmail.com>
2018-03-21 16:15:50 +0800

    xlsx1.php(stat) done

--- xlsx1.php
+++ xlsx1.php
@@ -9,52 +9,63 @@
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
+use PhpOffice\PhpSpreadsheet\Style\Border;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
 
 // print_r($tables);
 $spreadsheet = new Spreadsheet();
 
-foreach($tables as $k=>$table){
+foreach ($tables as $k => $table) {
 	$spreadsheet->createSheet();
 	$sheet=$spreadsheet->getSheet($k);
 	$sheet->setTitle("$table[1]");
 	$thead[0] = $table[1];
+	// thead;
 	$sheet->fromArray($thead, NULL);
-	$sheet->fromArray($table[2], NULL ,'A2');
+	// tbody;
+	$i=2;
+	foreach ($table[2] as $t) {
+		$sheet->setCellValueByColumnAndRow(1, $i, $t[$table[0]]);
+		$sheet->setCellValueByColumnAndRow(2, $i, $t['count']);
+		$sheet->setCellValueByColumnAndRow(3, $i, $t['count_wip']);
+		$sheet->setCellValueByColumnAndRow(4, $i, "=C{$i}/B{$i}");
+		$sheet->setCellValueByColumnAndRow(5, $i, $t['sum_plan']);
+		$sheet->setCellValueByColumnAndRow(6, $i, $t['sum_accum']);
+		$sheet->setCellValueByColumnAndRow(7, $i, "=F{$i}/E{$i}");
+		$i++;
+	}
+	// tfoot;
+	$ii = $i-1;
+	$sheet->setCellValueByColumnAndRow(1, $i, '合计');
+	$sheet->setCellValueByColumnAndRow(2, $i, "=SUM(B2:B{$ii})");
+	$sheet->setCellValueByColumnAndRow(3, $i, "=SUM(C2:C{$ii})");
+	$sheet->setCellValueByColumnAndRow(4, $i, "=C{$i}/B{$i}");
+	$sheet->setCellValueByColumnAndRow(5, $i, "=SUM(E2:E{$ii})");
+	$sheet->setCellValueByColumnAndRow(6, $i, "=SUM(F2:F{$ii})");
+	$sheet->setCellValueByColumnAndRow(7, $i, "=F{$i}/E{$i}");
+	//style;
+	// thead and tfoot bold;
+	$sheet->getStyle('A1:G1')->applyFromArray(['font'=>['bold'=>true]]);
+	$sheet->getStyle("A{$i}:G{$i}")->applyFromArray(['font'=>['bold'=>true]]);
+	// autowidth not working?
+	// $sheet->getColumnDimension('A')->setAutoSize(true);
+	// number format percent;
+	$sheet->getStyle("D2:D{$i}")->getNumberFormat()->setFormatCode(NumberFormat::FORMAT_PERCENTAGE_00);
+	$sheet->getStyle("G2:G{$i}")->getNumberFormat()->setFormatCode(NumberFormat::FORMAT_PERCENTAGE_00);
+	// borders and center horizontally to the whole sheet;
+	$style = [
+		'borders'=>[
+			'allBorders'=>[
+				'borderStyle'=>Border::BORDER_THIN
+			]
+		],
+		'alignment'=>[
+			'horizontal'=>Alignment::HORIZONTAL_CENTER
+		]
+	];
+	$sheet->getStyle("A1:G{$i}")->applyFromArray($style);
 }
-
-//$sheet=$spreadsheet->getActiveSheet();
-//$sheet->setTitle('项目类型');
-//$sheet->fromArray(['项目类型','项目个数','占 比'], NULL);
-//$sheet->fromArray($t_rows, NULL ,'A2');
-//
-//$spreadsheet->createSheet();
-//$sheet=$spreadsheet->getSheet(1);
-//$sheet->setTitle('建设性质');
-//$sheet->fromArray(['建设性质','项目个数','占12 比'], NULL);
-//$sheet->fromArray($p_rows, NULL ,'A2');
-//
-//$spreadsheet->createSheet();
-//$sheet=$spreadsheet->getSheet(2);
-//$sheet->setTitle('责任单位');
-//$sheet->fromArray(['责任单位','项目个数','占12 比'], NULL);
-//$sheet->fromArray($o_rows, NULL ,'A2');
-//
-//$spreadsheet->createSheet();
-//$sheet=$spreadsheet->getSheet(3);
-//$sheet->setTitle('投资主体');
-//$sheet->fromArray(['投资主体','项目个数','占12 比'], NULL);
-//$sheet->fromArray($ib_rows, NULL ,'A2');
-//
-//$spreadsheet->createSheet();
-//$sheet=$spreadsheet->getSheet(3);
-//$sheet->setTitle('总投资');
-//$sheet->fromArray(['总投资','项目个数','占12 比'], NULL);
-//$sheet->fromArray($a, NULL ,'A2');
-//$sheet->fromArray($b, NULL ,'A3');
-//$sheet->fromArray($c, NULL ,'A4');
-//$sheet->fromArray($d, NULL ,'A5');
-//$sheet->fromArray($e, NULL ,'A6');
-//$sheet->fromArray($f, NULL ,'A7');
 
 // $writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
 $writer = new Xlsx($spreadsheet);
