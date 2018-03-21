ecc00254e43d3997e389f3fd73a50c1c3152ecb9
Dot <dotcra@gmail.com>
2018-03-21 19:05:57 +0800

    soft link bootstrap.3.3.7 css and js

--- progress.php
+++ progress.php
@@ -443,7 +443,7 @@ if(isset($alert1)){
 <!-- The Canvas to Blob plugin is included for image resizing functionality -->
 <script src="https://blueimp.github.io/JavaScript-Canvas-to-Blob/js/canvas-to-blob.min.js"></script>
 <!-- Bootstrap JS is not required, but included for the responsive demo navigation -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="<?= $root ?>/js/bootstrap.3.3.7.min.js"></script>
 <!-- blueimp Gallery script -->
 <script src="https://blueimp.github.io/Gallery/js/jquery.blueimp-gallery.min.js"></script>
 <!-- The Iframe Transport is required for browsers without support for XHR file uploads -->
