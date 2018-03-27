13b3cece40f6e73c6120349287baabc13246b35d
Dot <dotcra@gmail.com>
2018-03-27 15:22:41 +0800

    add invest.php

---
+++ invest.php
@@ -0,0 +1,96 @@
+	  <div class="container" id="">
+		  <nav aria-label="breadcrumb" class="position-relative">
+				  <ol class="breadcrumb">
+				  <li class="breadcrumb-item"><a href="<?= "$root/home" ?>">首 页</a></li>
+				  <li class="breadcrumb-item active">固定资产投资</li>
+				  </ol>
+		  </nav>
+		<section class="row">
+			<aside class="col-md-auto">
+				<div class="list-group">
+				  <a href="#" class="list-group-item list-group-item-action">公告通知</a>
+				  <a href="<?= "$root/invest" ?>" class="list-group-item list-group-item-action active">固定资产投资</a>
+				  <a href="<?= "$root/project" ?>" class="list-group-item list-group-item-action">重点项目进展</a>
+				  <a href="<?= "$root/admin/chpwd" ?>" class="list-group-item list-group-item-action">设置</a>
+				</div>
+			</aside>
+
+		  <main class="col-md mt-2">
+	<table class="table table-bordered">
+		<thead>
+			<th colspan="10">
+				茅箭区2018年1-2月固定资产投资完成情况
+			</th>
+			<tr>
+				<th scope="col" colspan="2">指标</th>
+				<th scope="col">全区</th>
+				<th scope="col">武当</th>
+				<th scope="col">二堰</th>
+				<th scope="col">五堰</th>
+				<th scope="col">开发区</th>
+				<th scope="col">大川镇</th>
+				<th scope="col">塞武当</th>
+				<th scope="col">茅塔</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<th scope="row" rowspan="2">固定资产投资(万元)</th>
+				<td>总量</td>
+				<td>176752</td>
+				<td>21000</td>
+				<td>32384</td>
+				<td>55126</td>
+				<td>59542</td>
+				<td>3500</td>
+				<td>1300</td>
+				<td>3900</td>
+			</tr>
+			<tr>
+				<th scope="row">增幅%</th>
+				<td>15.3</td>
+				<td>16</td>
+				<td>15.2</td>
+				<td>16.1</td>
+				<td>9.4</td>
+				<td>-</td>
+				<td>2500</td>
+				<td>136.4</td>
+			</tr>
+			<tr>
+				<th scope="row" rowspan="3">施工项目个数</th>
+				<td>本月</td>
+				<td>2</td>
+				<td>-</td>
+				<td>-</td>
+				<td>1</td>
+				<td>1</td>
+				<td>-</td>
+				<td>-</td>
+				<td>-</td>
+			</tr>
+			<tr>
+				<td>累计</td>
+				<td>72</td>
+				<td>10</td>
+				<td>15</td>
+				<td>15</td>
+				<td>23</td>
+				<td>2</td>
+				<td>2</td>
+				<td>5</td>
+			</tr>
+			<tr>
+				<td>增减</td>
+				<td>22</td>
+				<td>4</td>
+				<td>-11</td>
+				<td>10</td>
+				<td>14</td>
+				<td>1</td>
+				<td>1</td>
+				<td>3</td>
+			</tr>
+		</tbody>
+	</table>
+	</main>
