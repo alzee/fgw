bc363ed8aa489094f657196380081b4158045a7c
Dot <dotcra@gmail.com>
2018-05-21 11:08:45 +0800

    nth

--- proc.php
+++ proc.php
@@ -15,11 +15,10 @@
   <input type="radio" id="customRadioInline4" name="customRadioInline1" class="custom-control-input">
   <label class="custom-control-label" for="customRadioInline4">无办理项</label>
 </div>
--->
 
 
 
-<table class="table table-borderless">
+<table class="table table-bordered">
 	<tbody>
 		<tr>
 			<th scope="col" rowspan="3">1</th>
@@ -43,5 +42,50 @@
 				</div>
 			</td>
 		</tr>
+
+		<tr>
+			<td>b:</td>
+			<td>
+				<div class="custom-control custom-radio custom-control-inline">
+					<input type="radio" id="customRadioInline1" name="customRadioInline1" class="custom-control-input">
+					<label class="custom-control-label" for="customRadioInline1">已办结</label>
+				</div>
+				<div class="custom-control custom-radio custom-control-inline">
+					<input type="radio" id="customRadioInline2" name="customRadioInline1" class="custom-control-input">
+					<label class="custom-control-label" for="customRadioInline2">办理中</label>
+				</div>
+				<div class="custom-control custom-radio custom-control-inline">
+					<input type="radio" id="customRadioInline3" name="customRadioInline1" class="custom-control-input">
+					<label class="custom-control-label" for="customRadioInline3">未办理</label>
+				</div>
+				<div class="custom-control custom-radio custom-control-inline">
+					<input type="radio" id="customRadioInline4" name="customRadioInline1" class="custom-control-input">
+					<label class="custom-control-label" for="customRadioInline4">无办理项</label>
+				</div>
+			</td>
+		</tr>
+
+		<tr>
+			<td>c:</td>
+			<td>
+				<div class="custom-control custom-radio custom-control-inline">
+					<input type="radio" id="customRadioInline1" name="customRadioInline1" class="custom-control-input">
+					<label class="custom-control-label" for="customRadioInline1">已办结</label>
+				</div>
+				<div class="custom-control custom-radio custom-control-inline">
+					<input type="radio" id="customRadioInline2" name="customRadioInline1" class="custom-control-input">
+					<label class="custom-control-label" for="customRadioInline2">办理中</label>
+				</div>
+				<div class="custom-control custom-radio custom-control-inline">
+					<input type="radio" id="customRadioInline3" name="customRadioInline1" class="custom-control-input">
+					<label class="custom-control-label" for="customRadioInline3">未办理</label>
+				</div>
+				<div class="custom-control custom-radio custom-control-inline">
+					<input type="radio" id="customRadioInline4" name="customRadioInline1" class="custom-control-input">
+					<label class="custom-control-label" for="customRadioInline4">无办理项</label>
+				</div>
+			</td>
+		</tr>
 	</tbody>
 </table>
+-->
