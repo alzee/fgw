68ae7655c8298298f374222c0199f63be157a984
Dot <dotcra@gmail.com>
2018-05-21 10:45:33 +0800

    use table in proc.php

--- proc.php
+++ proc.php
@@ -1,4 +1,4 @@
-<span>a</span>
+<!--
 <div class="custom-control custom-radio custom-control-inline">
   <input type="radio" id="customRadioInline1" name="customRadioInline1" class="custom-control-input">
   <label class="custom-control-label" for="customRadioInline1">已办结</label>
@@ -15,3 +15,33 @@
   <input type="radio" id="customRadioInline4" name="customRadioInline1" class="custom-control-input">
   <label class="custom-control-label" for="customRadioInline4">无办理项</label>
 </div>
+-->
+
+
+
+<table class="table table-borderless">
+	<tbody>
+		<tr>
+			<th scope="col" rowspan="3">1</th>
+			<td>a:</td>
+			<td>
+				<div class="custom-control custom-radio custom-control-inline">
+					<input type="radio" id="customRadioInline1" name="customRadioInline1" class="custom-control-input">
+					<label class="custom-control-label" for="customRadioInline1">已办结</label>
+				</div>
+				<div class="custom-control custom-radio custom-control-inline">
+					<input type="radio" id="customRadioInline2" name="customRadioInline1" class="custom-control-input">
+					<label class="custom-control-label" for="customRadioInline2">办理中</label>
+				</div>
+				<div class="custom-control custom-radio custom-control-inline">
+					<input type="radio" id="customRadioInline3" name="customRadioInline1" class="custom-control-input">
+					<label class="custom-control-label" for="customRadioInline3">未办理</label>
+				</div>
+				<div class="custom-control custom-radio custom-control-inline">
+					<input type="radio" id="customRadioInline4" name="customRadioInline1" class="custom-control-input">
+					<label class="custom-control-label" for="customRadioInline4">无办理项</label>
+				</div>
+			</td>
+		</tr>
+	</tbody>
+</table>
