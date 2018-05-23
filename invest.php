610d937e307105ab6871395941a0984c2a830647
Dot <dotcra@gmail.com>
2018-05-23 09:33:05 +0800

    modify data in invset.php

--- invest.php
+++ invest.php
@@ -19,7 +19,7 @@
 	<table class="table table-bordered table-responsive-sm">
 		<thead>
 			<th colspan="10">
-				茅箭区2018年1-2月固定资产投资完成情况
+				茅箭区2018年1-3月固定资产投资完成情况
 			</th>
 			<tr>
 				<th scope="col" colspan="2">指标</th>
@@ -37,56 +37,56 @@
 			<tr>
 				<th scope="row" rowspan="2">固定资产投资(万元)</th>
 				<td>总量</td>
-				<td>176752</td>
-				<td>21000</td>
-				<td>32384</td>
-				<td>55126</td>
-				<td>59542</td>
-				<td>3500</td>
-				<td>1300</td>
-				<td>3900</td>
+				<td>374921</td>
+				<td>57030</td>
+				<td>98910</td>
+				<td>82561</td>
+				<td>123520</td>
+				<td>7300</td>
+				<td>2700</td>
+				<td>2900</td>
 			</tr>
 			<tr>
 				<td>增幅%</td>
-				<td>15.3</td>
-				<td>16</td>
-				<td>15.2</td>
-				<td>16.1</td>
+				<td>18.4</td>
+				<td>17.8</td>
+				<td>18.1</td>
+				<td>18.4</td>
+				<td>18.2</td>
+				<td>32.7</td>
+				<td>31.7</td>
 				<td>9.4</td>
-				<td>−</td>
-				<td>2500</td>
-				<td>136.4</td>
 			</tr>
 			<tr>
 				<th scope="row" rowspan="3">施工项目个数</th>
 				<td>本月</td>
+				<td>6</td>
 				<td>2</td>
 				<td>−</td>
-				<td>−</td>
-				<td>1</td>
-				<td>1</td>
+				<td>2</td>
+				<td>2</td>
 				<td>−</td>
 				<td>−</td>
 				<td>−</td>
 			</tr>
 			<tr>
 				<td>累计</td>
-				<td>72</td>
-				<td>10</td>
+				<td>78</td>
+				<td>12</td>
 				<td>15</td>
-				<td>15</td>
-				<td>23</td>
+				<td>17</td>
+				<td>25</td>
 				<td>2</td>
 				<td>2</td>
 				<td>5</td>
 			</tr>
 			<tr>
 				<td>增减</td>
-				<td>22</td>
+				<td>17</td>
 				<td>4</td>
-				<td>-11</td>
-				<td>10</td>
-				<td>14</td>
+				<td>-12</td>
+				<td>5</td>
+				<td>15</td>
 				<td>1</td>
 				<td>1</td>
 				<td>3</td>
