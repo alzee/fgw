ef6dac5406c8fe93e56a6883969da03eacd4f6a0
Dot <dotcra@gmail.com>
2018-06-26 11:34:21 +0800

    basic table structure

--- pproc.php
+++ pproc.php
@@ -3,8 +3,29 @@ use App\Db;
 
 $oid = $_SESSION['oid'];
 // prepare data
-$sql = "select name from `procedure`";
+$sql = "select name from `procedure` where level=2";
 $procedures = (new Db)->query($sql);
+
+$sql = "select parent,num,code,name from `procedure`";
+$allprocs = (new Db)->query($sql);
+
+$son = $allprocs;
+// $pra = $allprocs;
+foreach ($allprocs as $k => $v){
+	if ($v['num'] == 0){
+		$pra[$v['parent']]=$v;
+		// unset($pra[$k]);
+		unset($son[$k]);
+	}
+	else {
+		// $pra[$v['parent']]['son'] = [];
+		$pra[$v['parent']]['son'][$v['num']] = $v;
+		// unset($pra[$k]);
+	}
+}
+// var_dump($son);
+// var_dump($allprocs);
+// var_dump($pra);
 ?>
 	  <div class="container">
 		  <nav>
@@ -31,10 +52,11 @@ $procedures = (new Db)->query($sql);
 		  </div>
 		  </div>
 		  <main class="mt-2" id="stat">
+<!--
 			<table class="table table-responsive table-sm table-striped table-bordered">
 				<thead class="thead-light">
 					<tr>
-						<th scope="col">#</th>
+						<th scope="col">项目编号</th>
 						<th scope="col">项目名称</th>
 <?php foreach($procedures as $v): ?>
 						<th scope="col"><?= $v['name'] ?></th>
@@ -44,6 +66,25 @@ $procedures = (new Db)->query($sql);
 				<tbody>
 				</tbody>
 			</table>
+-->
+
+		<table class="table table-bordered table-responsive">
+			<tbody>
+				<tr>
+						<th scope="col" rowspan="2">项目编号</th>
+						<th scope="col" rowspan="2">项目名称</th>
+		<?php foreach ($pra as $v): ?>
+		<th scope="col" colspan="<?= $itemc ?>"><?= $v['name'] ?></th>
+		<?php endforeach ?>
+				</tr>
+				<tr>
+		<?php foreach ($son as $v): ?>
+		<th scope="col"><?= $v['name'] ?></th>
+		<?php endforeach ?>
+				</tr>
+			</tbody>
+		</table>
+
 		  </main>
 		</div>
 		<script src="<?= $root ?>/js/xlsx.full.min.js"></script>
