b7981cdb92875f7a3685dd0f77f1ae4dffbcddf6
dot craft <dotcra@gmail.com>
2018-08-20 06:14:05 +0800

    add cron.sh

---
+++ cron.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+i=$(cat db_passwd)
+h=${i%%:*}
+c=${i#*:}
+u=${c%:*}
+p=${c#*:}
+d=~/w/itove/fgw/
+
+mysql-u$u -p$p -h $h fgw -e "update projects set alert=2"
+
+cd $d
+mysqldump -u$u -p$p -h $h fgw > sql/fgw.sql
+git add .
+git commit -m "mysqldump"
