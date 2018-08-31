61ea6c52573b8e676c2796c0918bee3911c0b6ee
dot craft <dotcra@gmail.com>
2018-09-01 05:50:25 +0800

    cd first

--- cron.sh
+++ cron.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 # 0 0 * * * /home/dot/w/itove/fgw/cron.sh
 
+d=~/w/itove/fgw/
+cd $d
+
 i=$(cat db_passwd)
 h=${i%%:*}
 c=${i#*:}
 u=${c%:*}
 p=${c#*:}
-d=~/w/itove/fgw/
 date=$(date +%d)
 
 if [ "$date" -eq 20 ]; then
 	mysql-u$u -p$p -h $h fgw -e "update projects set alert=2"
 fi
 
-cd $d
-
-echo $u $p $h
 mysqldump -u$u -p$p -h $h fgw > sql/fgw.sql
 git add .
 git commit -m "mysqldump"
