bd4de64cb01d24c92866ea324476e5ef74f17898
Arc Tod <ctod@msn.com>
2018-11-03 09:48:14 +0800

    also change pid for progress table

--- sql/offline_pid.sh
+++ sql/offline_pid.sh
@@ -14,5 +14,7 @@ a=$(mysql -uroot -p$pw fgw -N -e 'select pid from projects where online=0')
 
 for i in $a
 do
-	mysql -uroot -p$pw fgw -e "update projects set pid = 10$i where pid = $i"
+	echo $i
+	#mysql -uroot -p$pw fgw -e "update projects set pid = 10$i where pid = $i"
+	#mysql -uroot -p$pw fgw -e "update progress set pid = 10$i where pid = $i"
 done
