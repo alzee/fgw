133e21f3a2c766a489884e51ad753a3fee9991f4
Arc Tod <ctod@msn.com>
2018-11-03 09:18:53 +0800

    add script to change pid for offline projects

---
+++ sql/offline_pid.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# vim:ft=sh
+# pid add 1000 for offline projects
+############### Variables ###############
+
+############### Functions ###############
+
+############### Main Part ###############
+
+pw=dot
+
+a=$(mysql -uroot -p$pw fgw -N -e 'select pid from projects where online=0')
+
+for i in $a
+do
+	mysql -uroot -p$pw fgw -e "update projects set pid = 10$i where pid = $i"
+done
