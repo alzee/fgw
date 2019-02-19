087201eaf4fc492f1b6c6e1ad9e8ad70ebf9fc1c
Arc Zee <z@arcz.ee>
2019-02-19 18:29:40 +0800

    nth

--- sql/20190219.sh
+++ sql/20190219.sh
@@ -15,3 +15,12 @@ pw=${c#*:}
 
 ### all projects offline
 mysql -h$host -u$user -p$pw fgw -N -e 'update projects set online = 0'
+
+### pid add 180000 for offline projects, 18 means 2018
+a=$(mysql -h$host -u$user -p$pw fgw -N -e 'select pid from projects')
+
+for i in $a
+do
+	mysql -h$host -u$user -p$pw fgw -e "update projects set pid = $((i+180000)) where pid = $i"
+	mysql -h$host -u$user -p$pw fgw -e "update progress set pid = $((i+180000)) where pid = $i"
+done
