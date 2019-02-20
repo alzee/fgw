1329b791f908a730ff4c5e3c0ff126a8a6002f2d
Arc Zee <z@arcz.ee>
2019-02-20 09:03:48 +0800

    nth

--- xlsx2db.php
+++ xlsx2db.php
@@ -10,10 +10,10 @@ $type='Xlsx';
 //$inputFileName = __DIR__ . '/fgw.xls';
 $file = 'proj_2019.xlsx';
 $inputFileName = $file;
-$sheetname='工业项目';
-//$sheetname='商贸项目';
-//$sheetname='基础设施';
-//$sheetname='乡村振兴';
+// $sheetname='工业项目';
+// $sheetname='商贸项目';
+// $sheetname='基础设施';
+$sheetname='乡村振兴';
 //$sheetname='招商项目';
 
 $reader = IOFactory::createReader($type);
@@ -21,7 +21,7 @@ $reader->setLoadSheetsOnly($sheetname);
 $spreadsheet = $reader->load($inputFileName);
 
 //$sheetData = $spreadsheet->getActiveSheet()->toArray(null, true, true, true);
-$sheetData = $spreadsheet->getActiveSheet()->rangeToArray('A5:M29', null, true, true, true);
+$sheetData = $spreadsheet->getActiveSheet()->rangeToArray('A5:M16', null, true, true, true);
 //echo $spreadsheet->getSheetCount();
 $loadedSheetNames = $spreadsheet->getSheetNames();
 // var_dump($loadedSheetNames);
@@ -38,7 +38,7 @@ $mysqli->set_charset('utf8');
 
 $table = 'projects';
 foreach ($sheetData as $k=>$v){
-	$sql="insert into $table (pid,pname,property,intro,investment,invest_plan,start,finish,investby,p_incharge,oid,oid_serve) values(
+	$sql="insert into $table (pid,pname,property,intro,investment,invest_plan,start,finish,investby,type,p_incharge,oid,oid_serve) values(
 		'" .  trim($sheetData[$k]['A']) ."',
 		'" .  trim($sheetData[$k]['B']) ."',
 		'" .  trim($sheetData[$k]['C']) ."',
@@ -48,6 +48,7 @@ foreach ($sheetData as $k=>$v){
 		'" .  trim($sheetData[$k]['G']) ."',
 		'" .  trim($sheetData[$k]['H']) ."',
 		'NULL',
+		'" .  $sheetname ."',
 		'" .  trim($sheetData[$k]['J']) ."',
 		'" .  trim($sheetData[$k]['L']) ."',
 		'" .  trim($sheetData[$k]['M']) ."')";
