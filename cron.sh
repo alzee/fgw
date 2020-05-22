b1cc8613d551104a438a35c6b097543d69b2a60b
Arc Zee <z@arcz.ee>
2020-05-22 10:19:16 +0800

    nth

--- cron.sh
+++ cron.sh
@@ -8,7 +8,7 @@ cd $d
 date=$(date +%d)
 
 if [ "$date" -eq 20 ]; then
-	mysql -u$u -p$p -h $h fgw -e "update projects set alert=2"
+	mysql -u$user -p$pw -h $host $db -e "update projects set alert=2"
 fi
 
 mysqldump --skip-extended-insert -u$user -p$pw -h $host $db > fgw.sql
