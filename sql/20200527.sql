eccee8de854f5fc987a10cc0137f13d3c877f1a9
Arc Zee <z@arcz.ee>
2020-05-27 13:17:37 +0800

    nth

--- sql/20200527.sql
+++ sql/20200527.sql
@@ -2,4 +2,7 @@
 update projects set oid_serve = 75 where pid = 50;
 
 -- 92号项目 计划开工时间 由原 2019 改为 2020， 计划竣工时间 由原 2020 改为 2021
-update projects set start = '2020', finish = '2021' where pid = 92;
+--update projects set start = '2020', finish = '2021' where pid = 92;
+
+-- 92号项目 计划开工时间 由原 2020 改为 2019， 计划竣工时间 由原 2021 改为 2020 by 贺晓斌
+update projects set start = '2019', finish = '2020' where pid = 92;
