1072258e5e962838ca187fa6ac3c595dae681493
Arc Zee <z@arcz.ee>
2020-06-13 13:46:33 +0800

    nth

---
+++ Twig.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * vim:ft=php et ts=4 sts=4
+ * @author z14 <z@arcz.ee>
+ * @version
+ * @todo
+ */
+
+namespace App\Twig;
+
+require_once 'vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader('templates');
+$twig = new \Twig\Environment($loader, [
+    'cache' => 'var/cache',
+]);
+
+
+echo $twig->render('i.html');
