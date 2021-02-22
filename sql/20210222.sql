ac3dd3a0debcef01927e8039ebb0fdcbaca01437
Al Zee <z@alz.ee>
2021-02-22 12:03:07 +0800

    nth

--- sql/20210222.sql
+++ sql/20210222.sql
@@ -1,1 +1,5 @@
 alter table progress add progress_from_jan varchar(1000) default null after progress;
+
+update projects set oid_1 = 3 where pid = 29;
+update projects set oid_serve_1 = 3 where pid = 29;
+update projects set oid_serve_1 = 84 where pid between 84 and 87;
