ef6576d3008019369217af1620ccc2b840587b5c
Al Zee <z@alz.ee>
2021-02-23 11:57:21 +0800

    pproc should NOT be truncated

--- sql/truncate.sql
+++ sql/truncate.sql
@@ -1,4 +1,5 @@
 truncate table `projects`;
 truncate table `path`;
-truncate table `pproc`;
 truncate table `progress`;
+update pproc set `e-1` = 0, `e-2` = 0, `e-3` = 0, `e-4` = 0, `e-5` = 0, `k-1` = 0 , `k-2` = 0, `k-3` = 0, `k-4` = 0 , `k-5` = 0, `k-6` = 0, `k-7` = 0, `q-1` = 0, `w-1` = 0, `w-2` = 0, `w-3` = 0, `w-4` = 0;
+
