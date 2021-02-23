d3945be746bb068db21fc3981bf5222fdfef47aa
Al Zee <z@alz.ee>
2021-02-23 10:13:28 +0800

    nth

--- sql/20210223.sql
+++ sql/20210223.sql
@@ -1,3 +1,6 @@
 -- 区卫健局重复
 update projects set oid_serve = 9 where oid_serve = 67;
 update organization set oname = '003' where oid = 67;
+
+update organization set oname = '东城经济开发区税务分局' where oid = 22;
+update projects set oid_serve_1 = 22 where pid = 18;
