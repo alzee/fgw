18771a3199f4ceea6aa817a5b405da5cae94ed9a
Al Zee <z@alz.ee>
2021-06-23 12:34:52 +0800

    nth

---
+++ sql/20210623.sql
@@ -0,0 +1,7 @@
+update projects set online = 0 where pid = 111;
+update projects set p_incharge = '董祥吉' where pid = 109;
+update projects set p_incharge = '金菊' where pid = 110;
+update projects set p_incharge = '董祥吉' where pid = 112;
+update projects set p_incharge = '董祥吉' where pid = 113;
+update projects set p_incharge = '吕自宏' where pid = 120;
+update projects set oid_serve = 80 where pid = 121;
